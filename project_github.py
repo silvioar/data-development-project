6a3d6eafb7395a0e3c18a10ef2e280e7fff0f9a2
silvioar <71708004+silvioar@users.noreply.github.com>
2021-11-10 20:13:13 -0300

    Update logs

--- project_github.py
+++ project_github.py
@@ -13,14 +13,24 @@ import time
 import numpy as np
 from sqlalchemy import create_engine
 # import pymysql
+from datetime import datetime
 
+def print_log(text):
+    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M')
+    print('%s | %s' % (timestamp, text))
 
 tickers = 'bitcoin,chainlink,ethereum,reserve-rights-token,cardano,polkadot,crypto-com-chain'
 url = f'https://api.coingecko.com/api/v3/simple/price?ids={tickers}&vs_currencies=usd&include_market_cap=true&include_24hr_vol=true'
 headers = {"accept: application/json"}
 
+print_log("##############################################################################################")
+print_log("Comenzando ...")
+print_log("##############################################################################################")
+
+print_log('Haciendo la request...')
 r = requests.get(url = url, verify = False)
 output = r.json()
+print_log('Creando Dataframe...')
 df = pd.DataFrame(output).T
 
 def get_engine():
@@ -36,5 +46,7 @@ df.columns = [['moneda','precio','market_cap','volumen']]
 df.columns = df.columns.get_level_values(0)
 
 # Guardar en base
+print_log('Escribiendo en la base...')
 escribir_base(df, 'test')
-    
+
+print_log('Actualización terminada...')
